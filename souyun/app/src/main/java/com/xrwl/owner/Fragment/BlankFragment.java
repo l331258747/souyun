58585c1a2538d9f499e8b29eb962ce574bf8eac4
liguoqiang <331258747@qq.com>
2021-01-07 13:54:12 +0800

    首页不跟随蓝点

--- souyun/app/src/main/java/com/xrwl/owner/Fragment/BlankFragment.java
+++ souyun/app/src/main/java/com/xrwl/owner/Fragment/BlankFragment.java
@@ -426,6 +426,15 @@ public class BlankFragment extends Fragment implements LocationSource, AMapLocat
 
     private void initMap() {
 
+        //蓝点初始化
+        myLocationStyle = new MyLocationStyle();//初始化定位蓝点样式类myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE);//连续定位、且将视角移动到地图中心点，定位点依照设备方向旋转，并且会跟随设备移动。（1秒1次定位）如果不设置myLocationType，默认也会执行此种模式。
+        myLocationStyle.interval(2000); //设置连续定位模式下的定位间隔，只在连续定位模式下生效，单次定位模式下不会生效。单位为毫秒。
+        //aMap.getUiSettings().setMyLocationButtonEnabled(true);设置默认定位按钮是否显示，非必需设置。
+        myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE_NO_CENTER);//连续定位、且将视角移动到地图中心点，定位点依照设备方向旋转，并且会跟随设备移动。（1秒1次定位）默认执行此种模式。
+
+        myLocationStyle.showMyLocation(true);
+
+        aMap.setMyLocationStyle(myLocationStyle);//设置定位蓝点的Style
         //设置地图的放缩级别
         aMap.moveCamera(CameraUpdateFactory.zoomTo(12));
         // 设置定位监听
@@ -434,16 +443,6 @@ public class BlankFragment extends Fragment implements LocationSource, AMapLocat
         aMap.setMyLocationEnabled(true);
         // 设置定位的类型为定位模式，有定位、跟随或地图根据面向方向旋转几种
         aMap.setMyLocationType(AMap.LOCATION_TYPE_LOCATE);
-
-        //蓝点初始化
-        myLocationStyle = new MyLocationStyle();//初始化定位蓝点样式类myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE);//连续定位、且将视角移动到地图中心点，定位点依照设备方向旋转，并且会跟随设备移动。（1秒1次定位）如果不设置myLocationType，默认也会执行此种模式。
-        myLocationStyle.interval(2000); //设置连续定位模式下的定位间隔，只在连续定位模式下生效，单次定位模式下不会生效。单位为毫秒。
-        aMap.setMyLocationStyle(myLocationStyle);//设置定位蓝点的Style
-        //aMap.getUiSettings().setMyLocationButtonEnabled(true);设置默认定位按钮是否显示，非必需设置。
-        aMap.setMyLocationEnabled(true);// 设置为true表示启动显示定位蓝点，false表示隐藏定位蓝点并不进行定位，默认是false。
-        myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE);//连续定位、且将视角移动到地图中心点，定位点依照设备方向旋转，并且会跟随设备移动。（1秒1次定位）默认执行此种模式。
-
-        myLocationStyle.showMyLocation(true);
 
         initSearchRoute();
     }
